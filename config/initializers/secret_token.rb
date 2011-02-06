0f00e4433db39c44c84da3f860cf8d18bc28dd95
vwakhare <vwakhare@innovtechindia.com>
2011-02-06 13:18:51 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Innovtech::Application.config.secret_token = '482d7872115e0588e0e93241b5da83dcf3bd36c90e89d0e8cfe2ac9bff40b4ee1117bb8797f6fff97fbe5aaef41c7054fa51b9581d392fe251cb448317d53e33'
